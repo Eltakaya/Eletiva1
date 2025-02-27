8e817317d67698b95dee5cf2c929f8e749097e22
Eduardo <107195198+Eltakaya@users.noreply.github.com>
2025-02-27 15:46:16 -0300

    Lista2_9,10,11,12
    ex9,10,11,12

---
+++ Lista2/12ex.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lista2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+  <form method="post" class="container mt-5" action="12resp.php">
+                        
+                        <div class="mb-3 col-md-2">
+                            <label for="num1" class="form-label ">Entre com a base:</label>
+                            <input type="number" id="num1" name="num1" class="form-control " required="">
+                        </div>
+                    
+                        <div class="mb-3 col-md-2">
+                            <label for="num2" class="form-label ">Entre com o expoente</label>
+                            <input type="number" id="num2" name="num2" class="form-control " required="">
+                        </div>
+                    
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                    
+                </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
