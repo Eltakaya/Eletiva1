8af057db920e021e9c27bfd914c9ff121b3e8213
Eduardo <107195198+Eltakaya@users.noreply.github.com>
2025-03-13 17:19:37 -0300

    Lista3,4
    ex1 lista 3
    ex 1 2 3 da lista 4

---
+++ Lista3/1ex.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lista2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+  <form method="post" class="container mt-5" action="1resp.php">
+                        
+                        <div class="mb-3 col-md-2">
+                            <label for="num1" class="form-label ">Digite o numero 1: </label>
+                            <input type="number" id="num1" name="num1" class="form-control " required="">
+                        </div>
+
+                        <div class="mb-3 col-md-2">
+                            <label for="num2" class="form-label ">Digite o numero 2: </label>
+                            <input type="number" id="num2" name="num2" class="form-control " required="">
+                        </div>
+
+                        <div class="mb-3 col-md-2">
+                            <label for="num3" class="form-label ">Digite o numero 3: </label>
+                            <input type="number" id="num3" name="num3" class="form-control " required="">
+                        </div>
+
+                        <div class="mb-3 col-md-2">
+                            <label for="num4" class="form-label ">Digite o numero 4: </label>
+                            <input type="number" id="num4" name="num4" class="form-control " required="">
+                        </div>
+
+                        <div class="mb-3 col-md-2">
+                            <label for="num5" class="form-label ">Digite o numero 5: </label>
+                            <input type="number" id="num5" name="num5" class="form-control " required="">
+                        </div>
+
+                        <div class="mb-3 col-md-2">
+                            <label for="num6" class="form-label ">Digite o numero 6: </label>
+                            <input type="number" id="num6" name="num6" class="form-control " required="">
+                        </div>
+                        <div class="mb-3 col-md-2">
+                            <label for="num7" class="form-label ">Digite o numero 7: </label>
+                            <input type="number" id="num7" name="num7" class="form-control " required="">
+                        </div>
+
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                    
+                </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
